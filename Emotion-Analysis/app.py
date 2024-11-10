82159acddc0a71b5536f6664a8d20525dfd50ca3
C0LAAAAA <2075544813@qq.com>
2024-11-10 23:06:37 +0800

    first commit

---
+++ Emotion-Analysis/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from views.page import page
+
+app = Flask(__name__)
+
+app.register_blueprint(page.pb)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
